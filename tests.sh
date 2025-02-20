2dcd041ebe69be974957be2a3755128a02dbdb7a
mfgm2 <repom2@airmail.cc>
2025-02-21 03:06:00 +0400

    add --locked to tests.sh

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 [ -f Cargo.toml ] || exit 1
 git submodule update --recursive --init
-cargo t --release
-cargo t --test regression --release -- --nocapture --ignored
+cargo t --locked --release
+cargo t --locked --test regression --release -- --nocapture --ignored
 rm -r target/test_out/
 
